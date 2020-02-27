0479813f121cde356e2b57de074fdb2fb0c41650
Jamal Frank <jamaljfrank@gmail.com>
2020-02-27 22:42:56 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,29 @@
 class TweetsController < ApplicationController
 
-
+  get '/tweets' do
+    if logged_in?
+      @tweets = Tweet.all
+      erb :'tweets/tweets'
+    else
+      redirect to '/login'
+    end
+  end
+   
+  get '/tweets/new' do
+    if logged_in?
+      erb :'tweets/new'
+    else
+      redirect "/login"
+    end
+  end
+  
+  post '/tweets' do
+    user = User.find_by_id(session[:user_id])
+      if !params[:content].empty?
+        @tweet = Tweet.create(:content => params[:content], :user_id => user.id)
+      redirect to "/tweets/#{@tweet.id}"
+    else
+      redirect "/tweets/new"
+    end
+  end
 end
